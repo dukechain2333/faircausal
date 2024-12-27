591f7a817a266d27231e30965f97ab7e7f5c8277
William Qian <william.z.qian@gmail.com>
2024-12-27 16:33:13 -0500

    Finished nde function

--- src/faircausal/optimizing/ObjectFunctions.py
+++ src/faircausal/optimizing/ObjectFunctions.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, log_loss
 
 from faircausal.data.CausalDataReader import CausalDataReader
-from faircausal.utils.Dag import recursive_predict, classify_confounders_mediators
+from faircausal.utils.Dag import recursive_predict, classify_confounders_mediators, find_parents
 
 
 def negative_log_likelihood(causal_data: CausalDataReader):
@@ -99,4 +99,140 @@ def nde(causal_data: CausalDataReader, exposure: str):
     nde_value = np.mean(y_pred_at_exposure_1_mediator_0 - y_pred_at_exposure_0_mediator_0)
 
     return nde_value
+
+
+def predict_node(node, data_row, parameter_vector, parameter_mapping, predicted_values):
+    """
+    Predict the value for a single node given the parameter vector.
+
+    :param node: The node to predict.
+    :param data_row: A single row from the DataFrame.
+    :param parameter_vector: The parameter vector.
+    :param parameter_mapping: Mapping from nodes to parameter indices and parents.
+    :param predicted_values: Dictionary of already predicted values.
+    :return: Predicted value for the node.
+    """
+    if node in predicted_values:
+        return predicted_values[node]
+
+    mapping = parameter_mapping[node]
+    start = mapping['start']
+    end = mapping['end']
+    parents = mapping['parents']
+    params = parameter_vector[start:end]
+
+    intercept = params[0]
+    coefficients = params[1:]
+
+    # Get parent values; recursively predict if necessary
+    parent_values = []
+    for parent in parents:
+        parent_value = predict_node(parent, data_row, parameter_vector, parameter_mapping, predicted_values)
+        parent_values.append(parent_value)
+    parent_values = np.array(parent_values, dtype=np.float64)
+
+    linear_output = intercept + np.dot(coefficients, parent_values)
+
+    if mapping['type'] == 'categorical':
+        # Logistic regression
+        y_pred_prob = 1 / (1 + np.exp(-linear_output))
+        # Binary output: return probability or class label
+        return y_pred_prob
+    else:
+        # Linear regression
+        return linear_output
+
+
+def negative_log_likelihood_param(causal_data: CausalDataReader, parameter_vector, parameter_mapping):
+    """
+    Negative Log-Likelihood for the causal model using the parameter vector.
+
+    :param causal_data: CausalDataReader object
+    :param parameter_vector: Parameter vector
+    :param parameter_mapping: Mapping of parameters
+    :return: Negative Log-Likelihood
+    """
+    data = causal_data.data
+    total_nll = 0
+
+    for index, data_row in data.iterrows():
+        predicted_values = {}
+        nll = 0
+
+        for node in causal_data.causal_dag.keys():
+            actual_value = data_row[node]
+            y_pred = predict_node(node, data_row, parameter_vector, parameter_mapping, predicted_values)
+
+            if parameter_mapping[node]['type'] == 'categorical':
+                # For categorical variables, compute log-likelihood
+                y_true = actual_value
+                y_pred_prob = y_pred
+                # Avoid numerical issues
+                y_pred_prob = np.clip(y_pred_prob, 1e-10, 1 - 1e-10)
+                if y_true == 1:
+                    nll += -np.log(y_pred_prob)
+                else:
+                    nll += -np.log(1 - y_pred_prob)
+            else:
+                # For continuous variables, assume Gaussian distribution
+                y_true = actual_value
+                y_pred_mean = y_pred
+                sigma = 1
+                nll += 0.5 * np.log(2 * np.pi * sigma ** 2) + ((y_true - y_pred_mean) ** 2) / (2 * sigma ** 2)
+
+        total_nll += nll
+
+    return total_nll
+
+
+def nde_param(causal_data: CausalDataReader, parameter_vector, parameter_mapping, exposure: str):
+    """
+    Calculate the Natural Direct Effect (NDE) using the parameter vector.
+
+    :param causal_data: CausalDataReader object
+    :param parameter_vector: Parameter vector
+    :param parameter_mapping: Mapping of parameters
+    :param exposure: The name of the exposure variable.
+    :return: Estimated NDE value.
+    """
+    data = causal_data.data
+    outcome_variable = causal_data.outcome_variable
+
+    mediator_info = classify_confounders_mediators(causal_data.causal_dag, exposure, outcome_variable)
+    mediators = mediator_info['mediators']
+
+    nde_values = []
+
+    for index, data_row in data.iterrows():
+        predicted_values = {}
+
+        # Predict mediators at exposure=0
+        data_row_e0 = data_row.copy()
+        data_row_e0[exposure] = 0
+        predicted_mediators_e0 = {}
+        for mediator in mediators:
+            predicted_values_m = {}
+            m_pred = predict_node(mediator, data_row_e0, parameter_vector, parameter_mapping, predicted_values_m)
+            predicted_mediators_e0[mediator] = m_pred
+
+        # Predict outcome at exposure=1, mediators from exposure=0
+        data_row_e1_m0 = data_row.copy()
+        data_row_e1_m0[exposure] = 1
+        for mediator in mediators:
+            data_row_e1_m0[mediator] = predicted_mediators_e0[mediator]
+        predicted_values = {}
+        y_pred_e1_m0 = predict_node(outcome_variable, data_row_e1_m0, parameter_vector, parameter_mapping, predicted_values)
+
+        # Predict outcome at exposure=0, mediators from exposure=0
+        data_row_e0_m0 = data_row.copy()
+        data_row_e0_m0[exposure] = 0
+        for mediator in mediators:
+            data_row_e0_m0[mediator] = predicted_mediators_e0[mediator]
+        predicted_values = {}
+        y_pred_e0_m0 = predict_node(outcome_variable, data_row_e0_m0, parameter_vector, parameter_mapping, predicted_values)
+
+        nde = y_pred_e1_m0 - y_pred_e0_m0
+        nde_values.append(nde)
+
+    return np.mean(nde_values)
 
--- src/faircausal/optimizing/Optimizing.py
+++ src/faircausal/optimizing/Optimizing.py
@@ -1,34 +1,150 @@
+from sklearn.linear_model import LogisticRegression, LinearRegression
+
 from faircausal.optimizing.ObjectFunctions import *
 import numpy as np
-import pandas as pd
 from scipy.optimize import minimize
 
 
-def eval_f(causal_data: CausalDataReader, lambda_value: float, exposure: str):
-    return lambda_value * nde(causal_data, exposure) + negative_log_likelihood(causal_data)
+# def eval_f(causal_data: CausalDataReader, lambda_value: float, exposure: str):
+#     return lambda_value * nde(causal_data, exposure) + negative_log_likelihood(causal_data)
+#
+#
+# def optimize(causal_data: CausalDataReader, exposure: str):
+#
+#     lambda_vals = np.arange(0, 1501, 10)
+#     results = pd.DataFrame({'nde': np.zeros(len(lambda_vals)),
+#                             'nll': np.zeros(len(lambda_vals)),
+#                             'mse': np.zeros(len(lambda_vals)),
+#                             'lambda': lambda_vals})
+#
+#     for i, lambda_val in enumerate(lambda_vals):
+#         res = minimize(fun=eval_f,
+#                        x0=causal_data['beta_dict'],
+#                        args=(causal_data['data'], lambda_val, exposure),
+#                        method='COBYLA',
+#                        options={'tol': 1.0e-8,
+#                                 'maxiter': 10000})
+#
+#         causal_data['beta_dict'] = res.x
+#
+#         results.at[i, 'nde'] = nde(causal_data)
+#         results.at[i, 'nll'] = negative_log_likelihood(causal_data)
+#         results.at[i, 'loss'] = loss(causal_data)
+#
+#     return results
+
+def initialize_parameters(causal_data: CausalDataReader):
+    """
+    Initialize the parameter vector and mapping for optimization.
+
+    :param causal_data: CausalDataReader object
+    :return: parameter_vector, parameter_mapping
+    """
+    parameter_vector = []
+    parameter_mapping = {}
+    index = 0
+
+    for node in causal_data.causal_dag.keys():
+        parents = find_parents(causal_data.causal_dag, node)
+        num_parents = len(parents)
+        num_params = num_parents + 1  # including intercept
+
+        # For mapping, store the slice indices
+        parameter_mapping[node] = {
+            'start': index,
+            'end': index + num_params,
+            'parents': parents,
+            'type': 'categorical' if causal_data.data[node].dtype.name == 'category' or causal_data.data[node].nunique() == 2 else 'continuous'
+        }
+        index += num_params
+
+        # Extract initial parameters from the model if available
+        if node in causal_data.linear_models:
+            model = causal_data.linear_models[node]
+            if isinstance(model, LogisticRegression):
+                # For LogisticRegression
+                intercept = model.intercept_[0] if model.intercept_.shape else model.intercept_
+                coef = model.coef_[0]
+                params = np.concatenate(([intercept], coef))
+            elif isinstance(model, LinearRegression):
+                # For LinearRegression
+                intercept = model.intercept_
+                coef = model.coef_
+                params = np.concatenate(([intercept], coef))
+            else:
+                # Default initialization
+                params = np.zeros(num_params)
+        else:
+            # Default initialization
+            params = np.zeros(num_params)
+
+        parameter_vector.extend(params)
+
+    parameter_vector = np.array(parameter_vector)
+    return parameter_vector, parameter_mapping
 
 
-def optimize(causal_data: CausalDataReader, exposure: str):
+def eval_f(parameter_vector, causal_data: CausalDataReader, parameter_mapping, lambda_value: float, exposure: str):
+    """
+    Evaluate the penalized objective function.
 
-    lambda_vals = np.arange(0, 1501, 10)
-    results = pd.DataFrame({'nde': np.zeros(len(lambda_vals)),
-                            'nll': np.zeros(len(lambda_vals)),
-                            'mse': np.zeros(len(lambda_vals)),
-                            'lambda': lambda_vals})
+    :param parameter_vector: Parameter vector
+    :param causal_data: CausalDataReader object
+    :param parameter_mapping: Mapping of parameters
+    :param lambda_value: Penalty parameter
+    :param exposure: The name of the exposure variable
+    :return: Penalized objective function value
+    """
+    nll = negative_log_likelihood_param(causal_data, parameter_vector, parameter_mapping)
+    nde_value = nde_param(causal_data, parameter_vector, parameter_mapping, exposure)
+    penalty = lambda_value * abs(nde_value)
+    return nll + penalty
 
-    for i, lambda_val in enumerate(lambda_vals):
-        res = minimize(fun=eval_f,
-                       x0=causal_data['beta_dict'],
-                       args=(causal_data['data'], lambda_val, exposure),
-                       method='COBYLA',
-                       options={'tol': 1.0e-8,
-                                'maxiter': 10000})
+def optimize(causal_data: CausalDataReader, exposure: str, lambda_value: float):
+    """
+    Optimize the parameters to minimize the penalized objective function.
 
-        causal_data['beta_dict'] = res.x
+    :param causal_data: CausalDataReader object
+    :param exposure: The name of the exposure variable
+    :param lambda_value: Penalty parameter
+    :return: Optimized parameter vector and updated causal_data with new parameters
+    """
+    # Initialize parameters and mapping
+    parameter_vector, parameter_mapping = initialize_parameters(causal_data)
 
-        results.at[i, 'nde'] = nde(causal_data)
-        results.at[i, 'nll'] = negative_log_likelihood(causal_data)
-        results.at[i, 'loss'] = loss(causal_data)
+    # Define the objective function for optimization
+    def objective_func(params):
+        return eval_f(params, causal_data, parameter_mapping, lambda_value, exposure)
 
-    return results
+    # Perform optimization
+    result = minimize(objective_func, parameter_vector, method='COBYLA', options={'maxiter': 1000})
+
+    # Update the linear_models in causal_data with optimized parameters
+    optimized_params = result.x
+    for node in causal_data.causal_dag.keys():
+        mapping = parameter_mapping[node]
+        start = mapping['start']
+        end = mapping['end']
+        params = optimized_params[start:end]
+        intercept = params[0]
+        coefficients = params[1:]
+        parents = mapping['parents']
+
+        # Create new models with optimized parameters
+        if mapping['type'] == 'categorical':
+            # Logistic Regression
+            model = LogisticRegression()
+            model.intercept_ = np.array([intercept])
+            model.coef_ = np.array([coefficients])
+        else:
+            # Linear Regression
+            model = LinearRegression()
+            model.intercept_ = intercept
+            model.coef_ = coefficients
+
+        # Update the model in causal_data
+        causal_data.linear_models[node] = model
+
+    return optimized_params, causal_data
+
 
